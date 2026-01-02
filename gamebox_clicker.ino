6663227555d438d0c8feebdd1f1ab0a96f47208b
byfinger <60677875+jungwonwoong@users.noreply.github.com>
2026-01-02 22:16:18 +0900

    Initialize gamebox with menu and game states

---
+++ gamebox_clicker.ino
@@ -0,0 +1,126 @@
+#include <U8g2lib.h>
+#include <Arduino.h>
+#include "PongGame.h"
+#include "SnakeGame.h"
+#include "StairGame.h"
+#include "GameState.h"
+//#include "InfiniteStairs.h"
+
+// ================= OLED =================
+U8G2_SSD1306_128X64_NONAME_1_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE);
+
+// ================= BUTTON =================
+#define BTN_UP     5
+#define BTN_DOWN   3
+#define BTN_LEFT   2
+#define BTN_RIGHT  4
+#define BTN_SELECT 6
+#define BTN_BACK   7
+
+// ================= STATE =================
+// enum AppState {
+//   STATE_MENU,
+//   STATE_PONG,
+//   STATE_SNAKE
+// };
+
+volatile AppState appState = STATE_MENU;
+int menuIndex = 0;
+
+const char* menuItems[] = {
+  "PONG",
+  "SNAKE",
+  "STAIRS"
+};
+const uint8_t MENU_COUNT = 3;
+
+// ================= SETUP =================
+void setup()
+{
+  pinMode(BTN_UP,     INPUT_PULLUP);
+  pinMode(BTN_DOWN,   INPUT_PULLUP);
+  pinMode(BTN_LEFT,   INPUT_PULLUP);
+  pinMode(BTN_RIGHT,  INPUT_PULLUP);
+  pinMode(BTN_SELECT, INPUT_PULLUP);
+  pinMode(BTN_BACK,   INPUT_PULLUP);
+  randomSeed(analogRead(A7));
+  u8g2.begin();
+  u8g2.setFlipMode(2);
+  u8g2.setFont(u8g2_font_6x12_tf);
+
+  pongInit();
+  snakeInit();
+  stairInit();
+}
+
+// ================= LOOP =================
+void loop()
+{
+  switch (appState)
+  {
+    case STATE_MENU:
+      menuLoop();
+      break;
+
+    case STATE_PONG:
+      pong();
+      break;
+
+    case STATE_SNAKE:
+      snake();
+      break;
+
+    case STATE_STAIRS:
+      stairGame();
+      break;
+  }
+}
+void menuLoop()
+{
+  static unsigned long lastInput = 0;
+  if (millis() - lastInput < 150) return;
+
+  if (digitalRead(BTN_UP) == LOW)
+  {
+    menuIndex = (menuIndex - 1 + MENU_COUNT) % MENU_COUNT;
+    lastInput = millis();
+  }
+
+  if (digitalRead(BTN_DOWN) == LOW)
+  {
+    menuIndex = (menuIndex + 1) % MENU_COUNT;
+    lastInput = millis();
+  }
+
+  if (digitalRead(BTN_SELECT) == LOW)
+  {
+    if (menuIndex == 0) {pongInit();appState = STATE_PONG;}
+    if (menuIndex == 1) {snakeInit();appState = STATE_SNAKE;}
+    if (menuIndex == 2) {stairInit();appState = STATE_STAIRS;}
+    delay(200);
+  }
+
+  drawMenu();
+}
+
+void drawMenu()
+{
+  u8g2.firstPage();
+  do {
+    u8g2.drawFrame(0, 0, 128, 64);
+    u8g2.setCursor(36, 12);
+    u8g2.print("GAME MENU");
+
+    for (uint8_t i = 0; i < MENU_COUNT; i++)
+    {
+      u8g2.setCursor(40, 30 + i * 14);
+      if (i == menuIndex)
+        u8g2.print("> ");
+      else
+        u8g2.print("  ");
+
+      u8g2.print(menuItems[i]);
+    }
+  } while (u8g2.nextPage());
+}
+
